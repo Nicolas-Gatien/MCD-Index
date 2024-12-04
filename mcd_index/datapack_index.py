56a3d9325d84ef3693f2cf35c13997ad9ea865b4
Nicolas Gatien <nicolas.gatien6@gmail.com>
2024-12-04 14:20:44 -0500

    Add Necessary Endpoints

--- mcd_index/datapack_index.py
+++ mcd_index/datapack_index.py
@@ -1,7 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 index_blueprint = Blueprint("datapack_index", __name__)
 
-@index_blueprint.route('/get')
+@index_blueprint.route('/api/get', methods=['POST'])
 def get_datapack():
     return "Got Datapack"
+
+@index_blueprint.route('/api/add', methods=['POST'])
+def add_datapack():
+    return "Uploading Datapack"
+
+@index_blueprint.route('/upload')
+def upload_datapack():
+    return render_template('upload.html')
