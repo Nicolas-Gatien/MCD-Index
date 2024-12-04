7f4a6953afe452c762b30cbd22b4f7d1e701b899
Nicolas Gatien <nicolas.gatien6@gmail.com>
2024-12-04 14:50:06 -0500

    Add SQLAlchemy DB

--- mcd_index/__init__.py
+++ mcd_index/__init__.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+import os
+
+class Base(DeclarativeBase):
+    pass
 
 app = Flask(__name__)
+db = SQLAlchemy(model_class=Base)
 
 def create_app():
     from .home import home_blueprint
@@ -8,5 +16,9 @@ def create_app():
 
     from .datapack_index import index_blueprint
     app.register_blueprint(index_blueprint)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URI"]
+
+    db.init_app(app)
 
     return app
