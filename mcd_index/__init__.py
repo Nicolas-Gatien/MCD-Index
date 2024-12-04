0024b1133685f8ad8c0c7d9b70a2a3e93c75d247
Nicolas Gatien <49788382+Nicolas-Gatien@users.noreply.github.com>
2024-12-04 18:03:11 -0500

    Connect to Postgres Database with Flask SQLAlchemy (#4)
    * Installed Flask SQL-Alchemy
    
    * Add SQLAlchemy DB
    
    * Add Database URI
    
    * Create Tables
    
    * Remove Env Variable From Docker Run
    
    * Use getenv
    
    * Remove Env Variables for Testing
    
    * Add Env Variables Back
    
    * Move db Initialization
    
    * Add Psycopg2 to requirements
    
    * Use Psycopg2 Binary
    
    * Move Initialization
    
    * Move Config Out of Create App
    
    * Use Host Localhost

--- mcd_index/__init__.py
+++ mcd_index/__init__.py
@@ -1,6 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String
+
+import os
+
+class Base(DeclarativeBase):
+    pass
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI")
+db = SQLAlchemy(model_class=Base)
+
+class Datapack(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True)
+    path: Mapped[str] = mapped_column(unique=True)
 
 def create_app():
     from .home import home_blueprint
@@ -8,5 +23,10 @@ def create_app():
 
     from .datapack_index import index_blueprint
     app.register_blueprint(index_blueprint)
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
 
     return app
