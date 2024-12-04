67a25d1f43090617dd7c9de7b218d588a7cecb01
Nicolas Gatien <nicolas.gatien6@gmail.com>
2024-12-04 15:13:32 -0500

    Create Tables

--- mcd_index/__init__.py
+++ mcd_index/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String
 
 import os
 
@@ -9,6 +10,11 @@ class Base(DeclarativeBase):
 
 app = Flask(__name__)
 db = SQLAlchemy(model_class=Base)
+
+class Datapack(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True)
+    path: Mapped[str] = mapped_column(unique=True)
 
 def create_app():
     from .home import home_blueprint
@@ -20,5 +26,8 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URI"]
 
     db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
 
     return app
