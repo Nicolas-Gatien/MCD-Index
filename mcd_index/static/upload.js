1583f0521e5681e5a90e0a5cfa1f47734cc5a609
Nicolas Gatien <nicolas.gatien6@gmail.com>
2024-12-04 19:07:29 -0500

    Add Upload Javascript

---
+++ mcd_index/static/upload.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const uploadSubmit = document.getElementsByName('upload')
+    const uploadFile = document.getElementsByName('file')
+
+    console.log(uploadSubmit)
+    console.log(uploadFile)
+});
